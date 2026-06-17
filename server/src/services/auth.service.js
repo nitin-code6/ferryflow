e5af07b3b5e0b494809675e5ef8aaf8ba22e2f17
Nitin Kumar <nitbit710@gmail.com>
2026-06-17 23:40:23 +0530

    feat: add password hashing to registration

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,11 +1,25 @@
+const User = require("../models/user.model");
+const bcrypt = require('bcrypt')
 const registerUser = async (userData) => {
 
-    console.log("Register Service Hit");
+    const email = userData.email;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return {
+            message: "User already exists",
+            success: false,
+        }
+    }
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    const { name } = userData;
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    });
+    return user;
 
-    return {
-        success: true,
-        message: "Service Working"
-    };
+
 };
 
 module.exports = {
